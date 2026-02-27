a18fed3bcad2f612bfbc77211ebf327cee51c515
dung070424 <chuducdung4@gmail.com>
2026-02-27 09:30:34 +0700

    feat: Create sidebar component with navigation menu items.

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -11,10 +11,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class SidebarComponent {
     menuItems = [
         { label: 'Tổng quan', icon: 'M4 4h4v16H4zm6 6h4v10h-4zm6-6h4v16h-4z', path: '/tong-quan' },
-        { label: 'Quản lý lợn', icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z', path: '/quan-ly-lon' },
-        { label: 'Hồ sơ sức khỏe', icon: 'M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-8 14H9v-2H7v-2h2v-2h2v2h2v2h-2v2z', path: '/ho-so-suc-khoe' },
-        { label: 'Lịch cho ăn', icon: 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z', path: '/lich-cho-an' },
-        { label: 'Báo cáo', icon: 'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z', path: '/bao-cao' },
         { label: 'Quản lý Nhân viên', icon: 'M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z', path: '/quan-ly-nhan-vien' },
         { label: 'Quản lý Cám Ăn', icon: 'M21 16.5c0 .38-.21.71-.53.88l-7.9 4.44c-.16.12-.36.18-.57.18-.21 0-.41-.06-.57-.18l-7.9-4.44A.991.991 0 0 1 3 16.5v-9c0-.38.21-.71.53-.88l7.9-4.44c.16-.12.36-.18.57-.18.21 0 .41.06.57.18l7.9 4.44c.32.17.53.5.53.88v9zM12 4.15L6.04 7.5 12 10.85l5.96-3.35L12 4.15z', path: '/quan-ly-cam-an' },
         { label: 'Quản lý Kho Cám', icon: 'M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5', path: '/quan-ly-kho-cam' },
